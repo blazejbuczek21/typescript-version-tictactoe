cadd1fefcdfe268aa1984e2b8981ff3829ecfd1e
blazejbuczek21 <101830746+blazejbuczek21@users.noreply.github.com>
2023-12-17 21:51:48 +0100

    Add files via upload

---
+++ tictactoe/dist/app.js
@@ -0,0 +1,143 @@
+const cells = document.querySelectorAll(".cell");
+const info = document.getElementById("info");
+const restart = document.getElementById("restart");
+const computer = document.getElementById("computer");
+const players = document.getElementById("players");
+let board = Array(9).fill(null);
+const x = {
+    symbol: "X",
+    color: "red",
+};
+const o = {
+    symbol: "O",
+    color: "blue",
+};
+let player = x;
+let moves = 0;
+let isComputerPlayer;
+let isMakingMove = false;
+const winCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+const computerGame = () => {
+    if (isMakingMove === false) {
+        isComputerPlayer = true;
+        computer.classList.add("header_item_active");
+        players.classList.remove("header_item_active");
+        restartGame();
+        startGame();
+    }
+};
+const playerGame = () => {
+    if (isMakingMove === false) {
+        isComputerPlayer = false;
+        players.classList.add("header_item_active");
+        computer.classList.remove("header_item_active");
+        restartGame();
+        startGame();
+    }
+};
+const startGame = () => {
+    cells.forEach((cell) => cell.addEventListener("click", cellClicked));
+    info.style.color = player.color;
+    if (isComputerPlayer === false)
+        return (info.innerText = `Player ${player.symbol}'s turn`);
+    if (isComputerPlayer === true && player === x)
+        return (info.innerText = `Your turn ${player.symbol}`);
+    if (isComputerPlayer === true && player === o)
+        return (info.innerText = `${player.symbol} is thinking`);
+};
+const cellClicked = (e) => {
+    const target = e.target;
+    const id = parseInt(target.id);
+    if (!board[id] && !isMakingMove) {
+        moves++;
+        board[id] = player;
+        target.innerText = player.symbol;
+        target.style.color = player.color;
+        logic();
+    }
+};
+const logic = () => {
+    if (playerWon() !== false) {
+        cells.forEach((cell) => cell.removeEventListener("click", cellClicked));
+        info.innerText = `Player ${player.symbol} won`;
+        info.style.color = player.color;
+        return;
+    }
+    if (moves === 9) {
+        info.innerText = "Draw";
+        info.style.color = "black";
+        return;
+    }
+    player = player === x ? o : x;
+    info.innerText = `Player ${player.symbol}'s move`;
+    info.style.color = player.color;
+    if (player === o && isComputerPlayer === true) {
+        info.innerText = `${player.symbol} is thinking`;
+        info.style.color = player.color;
+        makeMove();
+        return;
+    }
+    if (player === x && isComputerPlayer === true) {
+        info.innerText = `Your turn ${player.symbol}`;
+        return;
+    }
+};
+const makeMove = () => {
+    isMakingMove = true;
+    setTimeout(() => {
+        let emptyCells = [];
+        for (let i = 0; i < board.length; i++) {
+            if (!board[i])
+                emptyCells.push(i);
+        }
+        let randomIndex = Math.floor(Math.random() * emptyCells.length);
+        let move = emptyCells[randomIndex];
+        board[move] = player;
+        cells[move].innerText = player.symbol;
+        cells[move].style.color = player.color;
+        moves++;
+        isMakingMove = false;
+        logic();
+    }, 1000);
+};
+const playerWon = () => {
+    for (const combinations of winCombinations) {
+        let [a, b, c] = combinations;
+        if (board[a] && board[a] === board[b] && board[b] === board[c]) {
+            paintCells(a, b, c);
+            return [a, b, c];
+        }
+    }
+    return false;
+};
+const paintCells = (a, b, c) => {
+    const winnerClass = player.symbol.toLowerCase() + "-winner";
+    cells[a].classList.add(winnerClass);
+    cells[b].classList.add(winnerClass);
+    cells[c].classList.add(winnerClass);
+};
+const restartGame = () => {
+    if (isMakingMove === false)
+        restartBody();
+};
+const restartBody = () => {
+    board.fill(null);
+    cells.forEach((cell) => {
+        cell.innerText = "";
+        cell.classList.remove("x-winner");
+        cell.classList.remove("o-winner");
+        moves = 0;
+        player = x;
+        startGame();
+    });
+};
+restart.addEventListener("click", restartGame);
